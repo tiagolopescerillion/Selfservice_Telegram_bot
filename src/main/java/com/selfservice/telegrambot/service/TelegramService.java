80049efaddf41601f466d7ed83e1a59cb0fd9622
tiagolopescerillion <tiago.lopes@cerillion.com>
2025-12-04 14:23:01 +0000

    Import Objects for null checks

--- src/main/java/com/selfservice/telegrambot/service/TelegramService.java
+++ src/main/java/com/selfservice/telegrambot/service/TelegramService.java
@@ -24,6 +24,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class TelegramService {
